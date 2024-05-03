8658b1c26c2c1e697bc2d2d0fb693a2c48bd59c8
Khaled Hussein <husseink1@southernct.edu>
2024-05-03 17:44:01 -0400

    linked list

--- Linked List/LinkedList.py
+++ Linked List/LinkedList.py
@@ -3,15 +3,16 @@ class Node:
         self.data = data # the data the node is referencing
         self.next = None # initially not pointing to any other node
 
-'''
-append(value)
-prepend(value)
-insert(value, index)
-'''
 class SinglyLinkedList:
     def __init__(self):
         self.head = None # Keep track of the head - empty on intialization
 
+    def is_empty(self):
+        current_node = self.head
+        if current_node == None:
+            return True
+        return False
+        
     # Add at the beginning of linked list - O(1)
     def prepend(self, value): 
         new_node = Node(value)
@@ -35,21 +36,27 @@ class SinglyLinkedList:
                 current_node = current_node.next
             current_node.next = new_node
     
-    # Add after a specific index - O(n)
+    # Add at a specific index - O(n)
     def insert(self, value, index):
         new_node = Node(value)
-        current_node = self.head
         i = 0
 
-        while current_node is not None:
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+            return
+        
+        current_node = self.head
+        while current_node != None:
+            prev_node = current_node
+            current_node = current_node.next
             if i == index:
+                prev_node.next = new_node
                 new_node.next = current_node.next
-                current_node.next = new_node
                 return
             elif i <= index:
                 i+=1
-                current_node = current_node.next
-        raise IndexError("Index out of bound!")
+        return
     
     def remove_head(self): # O(1)
         current_node = self.head
@@ -60,8 +67,8 @@ class SinglyLinkedList:
     def remove_tail(self): # O(n)
         current_node = self.head
         prev_node = None
-        if current_node == None:
-            raise IndexError("Can't remove from empty linked list!")
+        if self.is_empty():
+            raise IndexError("List is empty.")
         while current_node.next != None:
             prev_node = current_node
             current_node = current_node.next
@@ -72,8 +79,8 @@ class SinglyLinkedList:
         current_node = self.head
 
         # When the ll is empty or the value is the head
-        if current_node == None:
-            raise IndexError("Can't remove from empty linked list!")
+        if self.is_empty():
+            raise IndexError("List is empty.")
         elif current_node.data == value:
             self.head = current_node.next
             return "removed " + str(value) + " at index " + str(index)
@@ -95,7 +102,23 @@ class SinglyLinkedList:
         # When the value doesn't exist
         return "value not found."
     
+    def search(self, value): # O(n)
+        current_node = self.head
+        index = 0
 
+        if self.is_empty():
+            raise IndexError("List is empty.")
+        
+        while current_node.next != None:
+            if current_node.data == value:
+                return index
+            index +=1
+            current_node = current_node.next
+        
+        # When the value is the tail
+        if current_node.data == value:
+            return index
+        return -1
      
     def __str__(self):
         nodes = []
@@ -108,29 +131,16 @@ class SinglyLinkedList:
 # example usage
 ll = SinglyLinkedList()
 
-try:
-    ll.remove_head()
-except IndexError as e:
-    print(e)
-
 ll.append(2)
 ll.append(8)
 ll.append(99)
-
-ll.prepend(99)
+ll.append(728)
 
 print(ll)
 
 try:
-    ll.insert(666,2)
+    ll.insert(6696,1)
 except IndexError as e:
     print(e)
 
-ll.remove_tail()
-print(ll)
-
-print(ll.remove(666))
-print(ll)
-
-print(ll)
 print(ll)
