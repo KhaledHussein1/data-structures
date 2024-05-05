151c8e5fcb37b420afd6215c226e9da4212307fa
Khaled Hussein <husseink1@southernct.edu>
2024-05-04 23:28:10 -0400

    Optimized DLL

--- Linked List/double/DoublyLinkedList.py
+++ Linked List/double/DoublyLinkedList.py
@@ -11,7 +11,7 @@ class DoublyLinkedList:
         self.size = 0
 
     # Insertion
-    def prepend(self, value): # O(n)
+    def prepend(self, value): # O(1)
         new_node = Node(value)
         if self.is_empty():
             self.head = new_node
@@ -25,7 +25,7 @@ class DoublyLinkedList:
         self.size += 1
 
 
-    def append(self, value): # O(n)
+    def append(self, value): # O(1)
         new_node = Node(value)
 
         if self.is_empty():
@@ -40,64 +40,92 @@ class DoublyLinkedList:
         self.size += 1
 
     def insert(self, value, index): # O(n/2) -> O(n)
-        new_node = Node(value)
-
         if self.is_empty() or index > self.size - 1:
             raise IndexError("Index out of bound.")
         elif index == 0:
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
+            self.prepend(value)
         elif index == (self.size-1):
-            new_node.prev = self.tail
-            self.tail.next = new_node
-            self.tail = new_node
+            self.append(value)
+        else:
+            new_node = Node(value)
+            if index > (self.size // 2):
+                current_node = self.tail
+                for i in range((self.size - 1)- index):
+                    current_node = current_node.prev
+                new_node.next = current_node
+                new_node.prev = current_node.prev
+                new_node.prev.next = new_node
+                current_node.prev = new_node
+            else:
+                current_node = self.head
+                while index != 0:
+                    index -= 1
+                    current_node = current_node.next
+                new_node.next = current_node
+                new_node.prev = current_node.prev
+                new_node.prev.next = new_node
+                current_node.prev = new_node
+            self.size += 1
+        return
+
+    # Deletion
+    def remove_head(self): # O(1)
+        if self.is_empty():
+            raise IndexError("Can't remove from empty linked list!")
+        elif self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+        self.size -= 1
+
+    def remove_tail(self): # O(1)
+        if self.is_empty():
+            raise IndexError("Can't remove from empty linked list!")
+        elif self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+        self.size -= 1
+
+    def remove(self, index): # O(n/2) -> O(n)
+        if self.is_empty() or index > self.size - 1:
+            raise IndexError("Index out of bound.")
+        elif index == 0:
+            self.remove_head()
+        elif index == (self.size-1):
+            self.remove_tail()
         elif index > (self.size // 2):
             current_node = self.tail
             for i in range((self.size - 1)- index):
                 current_node = current_node.prev
-            new_node.next = current_node
-            new_node.prev = current_node.prev
-            new_node.prev.next = new_node
-            current_node.prev = new_node
+            current_node.prev.next = current_node.next
+            current_node.next.prev = current_node.prev
         else:
             current_node = self.head
             while index != 0:
                 index -= 1
                 current_node = current_node.next
-            new_node.next = current_node
-            new_node.prev = current_node.prev
-            new_node.prev.next = new_node
-            current_node.prev = new_node
-        self.size += 1
+            current_node.prev.next = current_node.next
+            current_node.next.prev = current_node.prev
+        self.size -= 1
         return
-
-    # Deletion
-    def remove_head(self):
-        pass
-
-    def remove_tail(self):
-        pass
-
-    def remove(self, index):
-        pass
 
     # Traversal
     def search(self, value):
-        pass
-
-    # Helper Functions
-    def length(self):
-        count = 0
         if self.is_empty():
-            return 0
-        current_node = self.head
-        while current_node is not None:
-            count += 1
-            current_node = current_node.next
-        return count
+            return False
+        else:
+            current_node = self.head
+            while current_node is not None:
+                if current_node.data == value:
+                    return True
+                current_node = current_node.next
+        return False
 
-    def is_empty(self):
+    # Helper Function
+    def is_empty(self): # O(1)
         if self.head is None:
             return True
         return False
@@ -114,17 +142,17 @@ dll = DoublyLinkedList()
 
 
 dll.prepend(12)
-dll.prepend(2)
-dll.prepend(3)
-dll.prepend(4)
-dll.prepend(5)
 
-dll.append(13)
-dll.append(14)
-dll.append(15)
-
-dll.insert(333, 7)
+dll.append(1)
+dll.append(3)
+dll.append(2)
+dll.append(6)
+dll.append(7)
+dll.append(9)
+dll.append(199)
+dll.remove(4)
 print(dll)
+
 
 
 
