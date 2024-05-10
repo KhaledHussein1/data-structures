258c1a50178f89681ad8e3143252395ef0f2fb15
Khaled Hussein <husseink1@southernct.edu>
2024-05-10 11:52:00 -0400

    LL-Queue

--- Queues/LL-Queue.py
+++ Queues/LL-Queue.py
@@ -1,5 +1,5 @@
 import sys
-#sys.path.append('')  
+sys.path.append('C:/Users/Masch/Desktop/Data Structures')  
 from LinkedList.double.DoublyLinkedList import DoublyLinkedList as DLL
 
 class Queue:
@@ -10,11 +10,30 @@ class Queue:
         self.dll.remove_head()
 
     def push(self, value):
-        self.dll.prepend(value)
+        self.dll.append(value)
 
     def is_empty(self):
         return self.dll.is_empty()
     
+    def __str__(self):
+        nodes = []
+        current = self.dll.head
+        while current is not None:
+            nodes.append(str(current.data))
+            current = current.next
+        return " <-- ".join(nodes)
+    
 q = Queue()
 
 print(q.is_empty())
+q.push(1)
+q.push(2)
+q.push(3)
+q.push(4)
+q.push(5)
+q.push(6)
+
+print(q)
+q.pop()
+
+print(q)
