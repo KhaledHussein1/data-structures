cacce0bec65dc99c3c0ff3b7892a8bd0c1c97cc7
Khaled Hussein <husseink1@southernct.edu>
2024-05-16 21:23:17 -0400

    bst

---
+++ Trees/BST.py
@@ -0,0 +1,6 @@
+'''
+Basic operations on a bst take time proportional to the height of
+the tree. For a complete binary tree with n nodes, operations run 
+O(logn). If the tree is a linear chain of n nodes, however, the same
+operations take O(n). red-black trees gurantee a height of O(logn).
+'''
