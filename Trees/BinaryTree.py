9e9193f0d3353d9e92e4a2254e90574e3e7b00d1
Khaled Hussein <husseink1@southernct.edu>
2024-05-22 23:21:43 -0400

    binary tree

--- Trees/BinaryTree.py
+++ Trees/BinaryTree.py
@@ -1,1 +1,36 @@
 # Binary Tree
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+        
+    def insert(self, value):
+        pass
+
+    def delete(self, value):
+        pass
+
+    # Traversal
+    def in_order():
+        pass
+    def pre_order():
+        pass
+    def post_order():
+        pass
+    def level_order():
+        pass
+
+    def get_size():
+        pass
+
+    def is_empty(self):
+        return not self.root
+
+    def get_height():
+        pass
+
